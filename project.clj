e70617e272f46aa8228b40a7ce6d3ddbd88ca121
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-26 17:11:15 +0100

    release 0.6.4

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject riemann-grid "0.6.3"
+(defproject riemann-grid "0.6.4"
   :description "grid like view of riemann alerts"
   :url "https://github.com/exoscale/riemann-grid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure       "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
+  :dependencies [[org.clojure/clojure       "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli     "0.3.1"]
-                 [compojure                 "1.1.3"]
+                 [compojure                 "1.2.1"]
                  [hiccup                    "1.0.5"]
-                 [ring/ring-json            "0.2.0"]
-                 [riemann-clojure-client    "0.2.9"]
-                 [ring/ring-core            "1.2.1"]
-                 [ring/ring-jetty-adapter   "1.2.1"]
-                 [ring/ring-devel           "1.2.1"]
+                 [ring/ring-json            "0.3.1"]
+                 [riemann-clojure-client    "0.2.11"]
+                 [ring/ring-core            "1.3.1"]
+                 [ring/ring-jetty-adapter   "1.3.1"]
+                 [ring/ring-devel           "1.3.1"]
                  [log4j/log4j               "1.2.17"
                   :exclusions [javax.mail/mail
                                javx.jms/jms
